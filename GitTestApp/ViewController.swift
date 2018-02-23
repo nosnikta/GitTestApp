45c070dc841e7452b5f36265f56e506930b3fad9
Ben Atkinson <benaatkinson@gmail.com>
2018-02-24 00:48:21 +1100

    I added a string

--- GitTestApp/ViewController.swift
+++ GitTestApp/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     
     var numberNow = Double()
+    let myString = "Pooh"
 
     override func viewDidLoad() {
         super.viewDidLoad()
